4ec41a460d97632eddeb37f7e852a934fd14ff11
user1 <doantam8924@gmail.com>
2019-07-17 18:21:52 +0900

    Commit

--- develop/src/js/main.js
+++ develop/src/js/main.js
@@ -16,42 +16,16 @@ $("#js-menuButton").on("click", function() {
  return false;
 });
 
-$('.nav a[href^="#"]').on("click",function() {
- var speed = 500;
- var href = $(this).attr("href");
- var target = $(href == "#" || href == "" ? "html" : href);
- var position = target.offset().top - 104;
- $("html, body").animate({ scrollTop: position }, speed, "swing");
- $("#js-menuButton").removeClass("active");
- if ($(window).width() < 768) {
-   $("#js-menu").hide();
- }
- return false;
-});
-
 // Anchor
 $('a[href^="#"]').click(function(){
   var hasttop = $(this.hash).offset().top;
-  var headerheight = $("#header").innerHeight();
-  var hasttopNew = hasttop - headerheight
+  var hasttopNew = hasttop - 104;
   $('html, body').animate({
     scrollTop: hasttopNew
   }, 500);
+  $("#js-menuButton").removeClass("active");
+  if ($(window).width() < 768) {
+      $("#js-menu").hide();
+    }
   return false;
 });
-
-// $(window).on('load scroll', function(){
-//   var elem = $('.animated');
-//   elem.each(function () {
-//     var isAnimate = $(this).data('animate');
-//     var elemOffset = $(this).offset().top;
-//     var scrollPos = $(window).scrollTop();
-//     var wh = $(window).height();
-//
-//     if(scrollPos > elemOffset - wh + (wh / 2)){
-//       $(this).addClass(isAnimate);
-//     }else{
-//       $(this).removeClass(isAnimate);
-//     }
-//   });
-// });
